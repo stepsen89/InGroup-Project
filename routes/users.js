e30158d5719c95e5e4562ccbbd8713bf37fc94fc
stepsen89 <stephanie.senoner@gmail.com>
2018-02-27 18:28:42 +0100

    routes users

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/user');
+
+router.get('/', (req, res, next) => {
+  User.find()
+    .then((result) => res.json(result))
+    .catch(next);
+});
+
+module.exports = router;
